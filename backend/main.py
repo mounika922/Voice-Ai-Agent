b256eb78e340f85fbf8abcdda4bb8de07f69f937
Mounika <mounikabarla123@gmail.com>
2026-03-06 20:51:51 +0530

    Voice-Ai-Agent

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,6 @@
-from fastapi import FastAPI, UploadFile
+from fastapi import FastAPI
 from services.language_detection import detect_language
-from services.speech_to_text import speech_to_text
+
 from services.response_formatter import format_response
 from agent.agent import interpret_request
 from scheduler.appointment_engine import book_appointment, cancel_appointment
@@ -55,51 +55,6 @@ def process_text(text: str):
     }
 
 
-@app.post("/voice")
-async def process_voice(file: UploadFile):
 
-    start = time.time()
-    session_id = "user1"
-
-    audio_path = f"temp_{file.filename}"
-
-    with open(audio_path, "wb") as f:
-        f.write(await file.read())
-
-    text = speech_to_text(audio_path)
-
-    language = detect_language(text)
-    intent = interpret_request(text)
-
-    if intent["intent"] == "book":
-
-        save_session(session_id, {"intent": "book"})
-        doctor = intent.get("doctor", "cardiologist")
-
-        response = book_appointment(doctor)
-
-        clear_session(session_id)
-
-    elif intent["intent"] == "cancel":
-
-        response = cancel_appointment()
-
-    else:
-        response = "I didn't understand"
-
-    response = format_response(response, language)
-
-    latency = (time.time() - start) * 1000
-
-    if os.path.exists(audio_path):
-        os.remove(audio_path)
-
-    return {
-        "text": text,
-        "language": language,
-        "response": response,
-        "latency_ms": latency
-    }
-   
 
   
--- services/speech_to_text.py
+++ services/speech_to_text.py
@@ -1,6 +1,6 @@
 import whisper
 
-model = whisper.load_model("base")
+model = whisper.load_model("tiny")
 
 def speech_to_text(audio_path):
     result = model.transcribe(audio_path)
